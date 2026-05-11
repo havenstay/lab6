cd8f0fc58352bca1789242acccd998b43227d3a0
Nikita <nik-ignatkov@mail.ru>
2026-05-11 22:32:01 +0800

    Update Model.h

--- Project1/Model.h
+++ Project1/Model.h
@@ -65,7 +65,7 @@ private:
 
     void processNode(aiNode* node, const aiScene* scene)
     {
-        // обработка мешей текущего узла
+
         for (unsigned int i = 0; i < node->mNumMeshes; ++i)
         {
             aiMesh* mesh = scene->mMeshes[node->mMeshes[i]];
@@ -73,7 +73,6 @@ private:
             meshes.push_back(processMesh(mesh, scene));
         }
 
-        // обработка дочерних узлов
         for (unsigned int i = 0; i < node->mNumChildren; ++i)
         {
             processNode(node->mChildren[i], scene);
@@ -85,17 +84,16 @@ private:
         vector<Vertex> vertices;
         vector<unsigned int> indices;
 
-        // вершины
+
         for (unsigned int i = 0; i < mesh->mNumVertices; ++i)
         {
             Vertex vertex;
 
-            // position
             vertex.Position.x = mesh->mVertices[i].x;
             vertex.Position.y = mesh->mVertices[i].y;
             vertex.Position.z = mesh->mVertices[i].z;
 
-            // normal
+
             if (mesh->HasNormals())
             {
                 vertex.Normal.x = mesh->mNormals[i].x;
@@ -106,7 +104,6 @@ private:
             vertices.push_back(vertex);
         }
 
-        // индексы
         for (unsigned int i = 0; i < mesh->mNumFaces; ++i)
         {
             aiFace face = mesh->mFaces[i];
